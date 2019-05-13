0fa8f5c2145707bd709c25d64163ffa2ddeb5362
zmoin <zarrin_617@yahoo.com>
2019-05-13 12:56:49 -0700

    updated index

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const Slapbot = require('./src/slapbot.js');
 let logger = require('./src/log.js');
 
 let slapbot = new Slapbot({
-    token: process.env.DISCORD_TOKEN,
+    token: DISCORD_TOKEN,
+    //if you want to run it locally
+    //token: process.env.DISCORD_TOKEN,
     whitelist: (process.env.WHITELIST_ENABLED || 'false').toLowerCase() == 'true',
     blacklist: (process.env.BLACKLIST_ENABLED || 'false').toLowerCase() == 'true'
 });

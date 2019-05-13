9da2e4c7c59a0dcb1141de4b00716a8fe00b47d5
zmoin <zarrin_617@yahoo.com>
2019-05-13 13:16:08 -0700

    bugfix for integration

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const Slapbot = require('./src/slapbot.js');
 let logger = require('./src/log.js');
 
 let slapbot = new Slapbot({
-    token: DISCORD_TOKEN,
-    //if you want to run it locally
-    //token: process.env.DISCORD_TOKEN,
+    token: process.env.DISCORD_TOKEN,
     whitelist: (process.env.WHITELIST_ENABLED || 'false').toLowerCase() == 'true',
     blacklist: (process.env.BLACKLIST_ENABLED || 'false').toLowerCase() == 'true'
 });
